357840db42396a9fc604ace24148abb20b6392ad
DebbiePotts87 <debbie.l.potts90@googlemail.com>
2024-02-09 12:41:24 +0000

    Fixed kittyball.js responses

--- assets/js/kitty-ball.js
+++ assets/js/kitty-ball.js
@@ -2,14 +2,14 @@ let randomNumber = Math.floor(Math.random() * 8);
 let text;
 
 const responses = [
-  "you're having a laugh",
-  "Don't be so silly",
-  "HAHAHAHAHA silly goose!",
+  "Get me some kibble and I'll tell you",
+  "MEOW MEOW!",
+  "No, have a nap instead",
   "Are you sure you really want to know that?",
-  "Not a chance I'm telling you that!",
-  "Why are you even playing this game?",
-  "You know, I'm getting bored now",
-  "I'm sorry but telling you the answer would be a really bad idea"
+  "Opening a can of tuna and you'll find the answer",
+  "HUSH now, I'm playing",
+  "I'll tell you when I'm done sleeping",
+  "The blanket holds the answers"
 ];
 
 if (randomNumber >= 0 && randomNumber < responses.length) {
